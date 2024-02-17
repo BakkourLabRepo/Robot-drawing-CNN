ce2d948010c5c0feed2740e8eff92c07ca4b9083
cty20010831 <congtianyue233@gmail.com>
2024-02-17 17:57:44 -0600

    Changed constants to UPPERCASE

--- Run_CNN_Model.py
+++ Run_CNN_Model.py
@@ -5,23 +5,37 @@
 ####################
 from Helper_Functions import *
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from keras.optimizers import Adam
-from keras.metrics import Recall, Precision, BinaryAccuracy
-#from keras.models import load_model
-from keras.callbacks import EarlyStopping
+from keras.models import load_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 #import matplotlib.pyplot as plt
 import sys
 import contextlib
+import argparse
 
+#########################################
+#                                       #
+#  Define command-line arguments used   #
+#                                       #
+#########################################
+parser = argparse.ArgumentParser(description='Process some inputs.')
+parser.add_argument('core', type=str, help='Modality: CPU vs GPU')
+parser.add_argument('--envrionment', type=str, choices=["local", "midway"], required=True, 
+                    help="Specify 'local' for running on a local machine or 'midway' for running on the Midway cluster."
+)
+parser.add_argument('--model_name', type=str, choices=["base", "VGG19"], required=True, help='The name of the model')
+
+args = parser.parse_args()
+
+core = args.core
+environment = args.environment
+model_name = args.model_name
 
 #########################################
 #                                       #
 # Define configuration and usage of GPU #
 #                                       #
 #########################################
-# Modality (CPU vs GPU)
+
 core = sys.argv[1]
 # Partition
 # partition = sys.argv[2]
@@ -59,52 +73,59 @@ save_frequency = base_save_freq * batch_size
 #                                       #
 #########################################
 # Body parts analyzed (e.g., arms + head + legs + antenna + feet)
-feature = ["bo", "he", "ar", "le", "an"]
-feature_num = len(feature)
+FEATURE = ["bo", "he", "ar", "le", "an"]
+FEATURE_NUM = len(FEATURE)
 # Number of options per class (e.g., 6 possible arms and legs for each robot)
-class_num = 6
-# All potential "feature * class_num" combinations of lables
-labels = [f"{f}_{n}" for f in feature for n in range(1, class_num + 1)]
-label_num = len(labels)
+CLASS_NUM = 6
+# All potential "FEATURE * CLASS_NUM" combinations of lables
+LABELS = [f"{f}_{n}" for f in FEATURE for n in range(1, CLASS_NUM + 1)]
 
 # Dataset spliting for model training, validation, and testing
-split_ratio = (0.8, 0.1, 0.1)
+SPLIT_RATIO = (0.8, 0.1, 0.1)
 # An integer for reproducibility of data spliting using sklearn.model_selection
-random_state = 42 
+RANDOM_STATE = 42 
 
 # Dropout rate
-dropout_rate = 0.4
+DROPOUT_RATE = 0.4
 
 # Setting a learning rate
-base_learning_rate = 0.01
+BASE_LEARNING_RATE = 0.01
 
 # A threshold value to decide which class the predicted probability points to
 # This threshold should take imbalanced 0s and 1s into account
-p_r_threshold = 0.3
+P_R_THRESHOLD = 0.3
 
 # Class weights to account for imbalanced dataset
-total_features = (class_num * feature_num)
-one_weight = (1 / feature_num) * (total_features / 2.0)
-zero_weight = (1 / (total_features - feature_num)) * (total_features / 2.0)
-positive_weight_body = 6
-positive_weight_other = 7 # higher because category can be missing
-positive_weights = [positive_weight_body]*class_num 
-positive_weights += [positive_weight_other]*(total_features - class_num) 
-positive_weights = tf.constant(positive_weights, dtype=tf.float32)
+TOTAL_FEATURES = (CLASS_NUM * FEATURE_NUM)
+# one_weight = (1 / FEATURE_NUM) * (TOTAL_FEATURES / 2.0)
+# zero_weight = (1 / (TOTAL_FEATURES - FEATURE_NUM)) * (TOTAL_FEATURES / 2.0)
+POSITIVE_WEIGHT_BODY = 6
+POSITIVE_WEIGHT_OTHER = 7 # higher because category can be missing
+POSITIVE_WEIGHTS = [POSITIVE_WEIGHT_BODY]*CLASS_NUM 
+POSITIVE_WEIGHTS += [POSITIVE_WEIGHT_OTHER]*(TOTAL_FEATURES - CLASS_NUM) 
+POSITIVE_WEIGHTS = tf.constant(POSITIVE_WEIGHTS, dtype=tf.float32)
+
+# Model checkpoint directory
+CHECKPOINT_DIR = "./model_checkpoint"
+# SavedModel directory
+SAVEDMODEL_DIR = "./Saved"
 
 #################
 #               #
 #  Load images  #
 #               #
 #################
-# Store the directory that stores stimulus in the midway environment
-project_path = "/project/bakkour/projects/feat_predict/"
-robots_stim_path = os.path.join(project_path, "robots", "stim/")
+# Define the `robots_stim_path` path storing robot drawings
+if environment == "midway":
+    project_path = "/project/bakkour/projects/feat_predict/"
+    robots_stim_path = os.path.join(project_path, "robots", "stim/")
+elif environment == "local":
+    robots_stim_path = "combs/"
 
-image_array, label_df = images_and_labels(robots_stim_path, feature_num, class_num, labels)
+image_array, label_df = images_and_labels(robots_stim_path, FEATURE_NUM, CLASS_NUM, LABELS)
 print(f"The shape of the image array is {image_array.shape}")
 print("-----------------------------------------------------------------------")
-print("The first five rows of feature vectors dataframe")
+print("The first five rows of FEATURE vectors dataframe")
 print(label_df.head())
 print("-----------------------------------------------------------------------")
 
@@ -115,10 +136,10 @@ print("-----------------------------------------------------------------------")
 #                   #
 #####################
 train_data, val_data, test_data = preprocessing(image_array, label_df, 
-                                                split_ratio, random_state,
+                                                SPLIT_RATIO, RANDOM_STATE,
                                                 batch_size)
 
-print("Diplay the first batch of images alongside their associated labels")
+print("Diplay the first batch of images alongside their associated LABELS")
 image_batch, label_batch = next(iter(train_data.take(1)))
 
 # Number of images to display from the batch
@@ -135,7 +156,7 @@ for i in range(num_images):
     feature_idx = np.where(feature_vec == 1)[0].tolist()
     string = ""
     for i in feature_idx:
-        string += (labels[i] + "_")
+        string += (LABELS[i] + "_")
     plt.title(string[:-1])
     plt.axis('off')
 """
@@ -147,40 +168,40 @@ print("-----------------------------------------------------------------------")
 #  Build and compile the model  #
 #                               #
 #################################
-
-# Build a simple CNN (using 5*5 filters)
-with strategy.scope():
-    model = Sequential([
-        Conv2D(32, (5, 5), activation='relu', input_shape=(750, 750, 1)),
-        MaxPooling2D(2, 2),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2),
-        Conv2D(128, (3, 3), activation='relu'),  # Additional Conv layer
-        MaxPooling2D(2, 2),
-        Flatten(),
-        Dense(256, activation='relu'),  # Increased units
-        Dropout(dropout_rate), 
-        Dense(128, activation='relu'),
-        Dropout(dropout_rate),  
-        Dense(30, activation='sigmoid')
-    ])
-
-    # Compile the CNN model
-    recall = Recall(thresholds=p_r_threshold)
-    precision = Precision(thresholds=p_r_threshold)
-    accuracy = BinaryAccuracy(threshold=p_r_threshold)
-
-    model.compile(loss=create_weighted_cross_entropy(positive_weights),
-                optimizer=Adam(learning_rate=base_learning_rate),
-                metrics=[recall, precision, accuracy])
-
-    # Early stopping callback
-    early_stopping = EarlyStopping(monitor='val_loss', 
+# Early stopping callback
+early_stopping = EarlyStopping(monitor='val_loss', 
                                 patience=3, verbose=1, mode='min', 
                                 restore_best_weights=True)
+
+# Define checkpoint callback
+model_checkpoint_callback = ModelCheckpoint(
+    filepath='model_checkpoint/model-{epoch:02d}-{val_loss:.2f}.h5',
+    save_weights_only=False,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True,
+    verbose=1)
+
+with strategy.scope():
+    # Reload last checkpoint and epoch in case of interruption
+    last_checkpoint, last_epoch = find_last_checkpoint(CHECKPOINT_DIR)
+
+    if last_checkpoint:
+        model = load_model(last_checkpoint)  # Load the last model
+        print(f"Resuming training from epoch {last_epoch + 1}")
+    else:
+        # Create and compile a new model
+        print("Starting training from scratch.")
+        model = compile_model(model = "base", 
+                              dropout_rate=DROPOUT_RATE, 
+                              p_r_threshold=P_R_THRESHOLD, 
+                              positive_weights=POSITIVE_WEIGHTS, 
+                              base_learning_rate=BASE_LEARNING_RATE)
+        last_epoch = 0
     
-    model.fit(train_data, epochs=20, 
-          validation_data=val_data, callbacks=[early_stopping])
+    model.fit(train_data, epochs=10, initial_epoch=last_epoch,
+              validation_data=val_data, 
+              callbacks=[early_stopping, model_checkpoint_callback])
 
 ##########################################
 #                                        #
@@ -215,25 +236,25 @@ with open("Model Performance.txt", "w") as f:
         print("---------------------------------------------------------------", 
               file=f)
 
-        print("Use this model to predict labels from test_data images", file=f)
+        print("Use this model to predict LABELS from test_data images", file=f)
         predictions = model.predict(test_data)
-        binary_prediction = np.where(predictions > p_r_threshold, 1, 0)
-        _, _, true_labels = data_split(label_df, split_ratio, 
-                                    random_state=random_state)
+        binary_prediction = np.where(predictions > P_R_THRESHOLD, 1, 0)
+        _, _, true_labels = data_split(label_df, SPLIT_RATIO, 
+                                       RANDOM_STATE=RANDOM_STATE)
         print("---------------------------------------------------------------", 
               file=f)
 
         print("Evaluate the prediction", file=f)
         classifi_report, confusion_matrix, roc_auc = \
-            test_predict_eval(binary_prediction, true_labels, feature_num, 
-                            class_num, labels)
+            test_predict_eval(binary_prediction, true_labels, FEATURE_NUM, 
+                              CLASS_NUM, LABELS)
 
         print(classifi_report, file=f)
         print("---------------------------------------------------------------", 
               file=f)
 
-# Save predicted and true labels for robots on testing data
-binary_prediction = pd.DataFrame(binary_prediction, columns=labels)
+# Save predicted and true LABELS for robots on testing data
+binary_prediction = pd.DataFrame(binary_prediction, columns=LABELS)
 predicted_labels_string = binary_prediction.apply(\
     lambda x: "_".join(list(np.array(binary_prediction.columns)[x == 1])), axis=1)
 true_labels_string = true_labels.apply(\
